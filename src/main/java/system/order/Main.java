d65977ecae0e6e08df8690671391c00c65ece924
dimak <dimakrjukov@gmail.com>
2025-01-22 19:21:48 +0100

    trying to make the method easier

--- src/main/java/system/order/Main.java
+++ src/main/java/system/order/Main.java
@@ -9,7 +9,13 @@ public class Main {
         boolean continueOrEx = true;
 
         while (continueOrEx) {
-            calcWall();
+            System.out.print("Введите цифру 1.Резание 2.Бурение: ");
+            int choice = scanner.nextInt();
+            if (choice == 1) {
+                calcRez();
+            } else {
+                calcBur();
+            }
 
             System.out.println("Хотите выполнить программу снова? (y/n): ");
             String userInput = scanner.nextLine().trim().toLowerCase();
@@ -23,9 +29,7 @@ public class Main {
     }
 
 
-
-    //Калькулятор
-    public static void calcWall() {
+    public static void calcRez() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Ширина стены в см: ");
@@ -49,8 +53,154 @@ public class Main {
                 System.out.println("Цена будет " + meter + "e");
             }
         } else {
-            System.out.println("Вышло меньше 200e, нужно минимум 200e для выезда.");
+            System.out.println("Вышло "+ meter+" ,а нужно минимум 200e для выезда.");
         }
+    }
+
+    public static void calcBur() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Самое популярное " +
+                "110 za ventilaciju u kupatilo," +
+                "120 dimnjak," +
+                " 130 aspirator");
+
+        double cm;
+        double result;
+        System.out.print("Какой фи? 1метр 100см: ");
+        int fi = scanner.nextInt();
+        switch (fi) {
+            case 50, 60, 70, 80 -> {
+                System.out.println("1.25e cm");
+                System.out.print("Сколько см нужно?: ");
+                double price = 1.25;
+                cm = scanner.nextFloat();
+                result = cm * price;
+                if (result > 500) {
+                    double discount = (result * 10) / 100;
+                    int priceWithDiscount = (int) (result - discount);
+                    System.out.println(result + "е это большой проект скидка будет 10 процентов" + priceWithDiscount + "e ");
+                }else System.out.println("Вышло " + result + "e ");
+            }
+            case 100 -> {
+                System.out.println("2.5e cm");
+                System.out.print("Сколько см нужно?: ");
+                cm = scanner.nextFloat();
+                result = cm * 2.5f;
+                if (result > 500) {
+                    double discount = (result * 10) / 100;
+                    int priceWithDiscount = (int) (result - discount);
+                    System.out.println(result + "е это большой проект скидка будет 10 процентов" + priceWithDiscount + "e ");
+                }else System.out.println("Вышло " + result + "e ");
+            }
+            case 110 -> {
+                System.out.println("2.75e cm");
+                System.out.print("Сколько см нужно?: ");
+                cm = scanner.nextFloat();
+                result = cm * 2.75f;
+                if (result > 500) {
+                    double discount = (result * 10) / 100;
+                    int priceWithDiscount = (int) (result - discount);
+                    System.out.println(result + "е это большой проект скидка будет 10 процентов" + priceWithDiscount + "e ");
+                }else System.out.println("Вышло " + result + "e ");
+            }
+            case 120 -> {
+                System.out.println("3e cm");
+                System.out.print("Сколько см нужно?: ");
+                cm = scanner.nextFloat();
+                result = cm * 3f;
+                if (result > 500) {
+                    double discount = (result * 10) / 100;
+                    int priceWithDiscount = (int) (result - discount);
+                    System.out.println(result + "е это большой проект скидка будет 10 процентов" + priceWithDiscount + "e ");
+                }else System.out.println("Вышло " + result + "e ");
+            }
+            case 130 -> {
+                System.out.println("3.25e cm");
+                System.out.print("Сколько см нужно?: ");
+                cm = scanner.nextFloat();
+                result = cm * 3.25f;
+                if (result > 500) {
+                    double discount = (result * 10) / 100;
+                    int priceWithDiscount = (int) (result - discount);
+                    System.out.println(result + "е это большой проект скидка будет 10 процентов" + priceWithDiscount + "e ");
+                }else System.out.println("Вышло " + result + "e ");
+            }
+            case 160 -> {
+                System.out.println("4e cm");
+                System.out.print("Сколько см нужно?: ");
+                cm = scanner.nextFloat();
+                result = cm * 4f;
+                if (result > 500) {
+                    double discount = (result * 10) / 100;
+                    int priceWithDiscount = (int) (result - discount);
+                    System.out.println(result + "е это большой проект скидка будет 10 процентов" + priceWithDiscount + "e ");
+                }else System.out.println("Вышло " + result + "e ");
+            }
+            case 180 -> {
+                System.out.println("4.5e cm");
+                System.out.print("Сколько см нужно?: ");
+                cm = scanner.nextFloat();
+                result = cm * 4.5f;
+                if (result > 500) {
+                    double discount = (result * 10) / 100;
+                    int priceWithDiscount = (int) (result - discount);
+                    System.out.println(result + "е это большой проект скидка будет 10 процентов" + priceWithDiscount + "e ");
+                }else System.out.println("Вышло " + result + "e ");
+            }
+            case 200 -> {
+                System.out.println("5e cm");
+                System.out.print("Сколько см нужно?: ");
+                cm = scanner.nextFloat();
+                result = cm * 5f;
+                if (result > 500) {
+                    double discount = (result * 10) / 100;
+                    int priceWithDiscount = (int) (result - discount);
+                    System.out.println(result + "е это большой проект скидка будет 10 процентов" + priceWithDiscount + "e ");
+                }else System.out.println("Вышло " + result + "e ");
+            }
+            case 250 -> {
+                System.out.println("6.25e cm");
+                System.out.print("Сколько см нужно?: ");
+                cm = scanner.nextFloat();
+                result = cm * 6.25f;
+                if (result > 500) {
+                    double discount = (result * 10) / 100;
+                    int priceWithDiscount = (int) (result - discount);
+                    System.out.println(result + "е это большой проект скидка будет 10 процентов" + priceWithDiscount + "e ");
+                }else System.out.println("Вышло " + result + "e ");
+            }
+            case 300 -> {
+                System.out.println("7.5e cm");
+                System.out.print("Сколько см нужно?: ");
+                cm = scanner.nextFloat();
+                result = cm * 7.5f;
+                if (result > 500) {
+                    double discount = (result * 10) / 100;
+                    int priceWithDiscount = (int) (result - discount);
+                    System.out.println(result + "е это большой проект скидка будет 10 процентов" + priceWithDiscount + "e ");
+                }else System.out.println("Вышло " + result + "e ");
+            }
+            case 350 -> {
+                System.out.println("8.75e cm");
+                System.out.print("Сколько см нужно?: ");
+                cm = scanner.nextFloat();
+                result = cm * 8.75f;
+                if (result > 500) {
+                    double discount = (result * 10) / 100;
+                    int priceWithDiscount = (int) (result - discount);
+                    System.out.println(result + "е это большой проект скидка будет 10 процентов" + priceWithDiscount + "e ");
+                }else System.out.println("Вышло " + result + "e ");
+            }
+            default -> System.out.println("Похоже вы указали неверное число");
+
+
+
+        }
+    }
+
+    private static void extracted(float result) {
+        System.out.println(result + "е это большой проект скидка будет 10 процентов");
     }
 
 }
